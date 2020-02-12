95d1ba812d0d27456c7efe5dc951d9cb77d8ff2b
Mathias7120 <42300995+Mathias7120@users.noreply.github.com>
2020-02-12 11:07:01 +0100

    L3

--- FirstProject.m
+++ FirstProject.m
@@ -1,6 +1,6 @@
 rosshutdown
 setenv('ROS_MASTER_URI','http://192.168.1.200:11311')
-setenv('ROS_IP','192.168.1.1')
+setenv('ROS_IP','192.168.1.100')
 rosinit('http://192.168.1.200:11311');
 
 velocity = 0.1;
