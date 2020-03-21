c6c8954227744d382e82d00bb18fd1b91ee2e0df
Molzen <33835030+Molzen@users.noreply.github.com>
2020-03-21 15:28:49 +0100

    Made adjustments to alogritm

--- L7-8 Point cloud/RangeAngleCoordinates.m
+++ L7-8 Point cloud/RangeAngleCoordinates.m
@@ -4,15 +4,24 @@ function distanceAndAngleToClosestPoint = RangeAngleCoordinates()
 
     scan = linescan;
 
+    ranges = linescan.Ranges; % Extract scan
+    angles = linescan.AngleMin:linescan.AngleIncrement:linescan.AngleMax;
+    angles = angles';
+    
+    meas = [ranges, angles];
     % get data from scan
-    cart = readCartesian(scan);
-    if(isempty(cart))
+    if(isempty(scan))
         distanceAndAngleToClosestPoint = [];
     else
-        closestPoint = cart(1,:);
-        for i = 2 : size(cart)
-            if(closestPoint(1,1) > cart(i,1))
-                closestPoint = cart(i,:);
+        for i = 1 : size(meas)
+            if(~isnan(meas(i,1)))
+                closestPoint = meas(i,:);
+                break
+            end
+        end
+        for i = 2 : size(meas)
+            if(closestPoint(1,1) > meas(i,1) && ~isnan(meas(i,1)))
+                closestPoint = meas(i,:);
             end
         end
         closestPoint(1,2) = rad2deg(closestPoint(1,2));
