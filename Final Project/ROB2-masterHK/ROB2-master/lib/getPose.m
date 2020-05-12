87e377d5d16514f9460707c183913ffea9e66247
Mathias7120 <42300995+Mathias7120@users.noreply.github.com>
2020-05-12 16:48:07 +0200

    Removed unnecessary files

--- Final Project/ROB2-masterHK/ROB2-master/lib/getPose.m
+++
@@ -1,7 +0,0 @@
-function [pose] = getPose(odom)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-odomdata = receive(odom,3);
-pose = odomdata.Pose.Pose;
-end
-
--- Final Project/ROB2-masterHK/ROB2-master/lib/move.m
+++
@@ -1,9 +0,0 @@
-function [] = move(vel,ang_vel,velpub)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
-velmsg = rosmessage(velpub);
-velmsg.Linear.X = vel;
-velmsg.Angular.Z = ang_vel;
-send(velpub,velmsg);
-end
-
