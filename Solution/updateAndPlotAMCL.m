ed5fb40f288d5f190badc0fd9ffb835bc9827277
Molzen <33835030+Molzen@users.noreply.github.com>
2020-06-09 11:00:03 +0200

    Update updateAndPlotAMCL.m

--- Solution/updateAndPlotAMCL.m
+++ Solution/updateAndPlotAMCL.m
@@ -1,8 +1,8 @@
 function [estimatedPosition, iterations] = updateAndPlotAMCL(amcl, lidarScan, currentPosition, iterations, visualizationHelper)
-%% Taken from "Localize TurtleBot Using Monte Carlo Localization"
-[isUpdated, estimatedPosition, ~] = amcl(currentPosition, lidarScan);
-if(isUpdated)
-    iterations = iterations + 1;
-    plotStep(visualizationHelper, amcl, estimatedPosition, lidarScan, iterations);
-end    
+    %% Taken from "Localize TurtleBot Using Monte Carlo Localization"
+    [isUpdated, estimatedPosition, ~] = amcl(currentPosition, lidarScan);
+    if(isUpdated)
+        iterations = iterations + 1;
+        plotStep(visualizationHelper, amcl, estimatedPosition, lidarScan, iterations);
+    end    
 end
